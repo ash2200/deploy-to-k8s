fe7d7472ec35eb84ae6613d97b631dab2212c07e
Cloud User <centos@ip-172-31-33-122.ap-south-1.compute.internal>
2021-09-26 12:00:04 +0000

    added deploy.sh and html folder and docker file to deploy onto GKE

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+From nginx
+
+WORKDIR '/app'
+
+EXPOSE 80
+
+COPY ./html/ /usr/share/nginx/html/
+
+
---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#Get the hash
+GIT_SHA = $(git rev-parse HEAD)
+
+#build docker image with the recent git commit hash
+
+docker build -t ash2200/static-website-k8s:$GIT_SHA .
+
+#Push the image to the docker hub
+
+docker push ash2200/static-website-k8s:$GIT_SHA
+
+#apply k8s yaml configs to form k8s resources
+
+kubectl apply -f k8s/namespace.yml
+
+kubectl apply -f k8s/
+
+#Update the image in deployment resources
+
+kubectl set image deployment.apps/static-web-deployment -n=sample days-app-container=ash2200/static-website-k8s:$GIT_SHA
+
+
+
